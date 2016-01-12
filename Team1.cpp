6a2d10068f53c685d759c23ac838d54da4808a10
MrOctantis <bob1nilly@gmail.com>
2016-01-11 17:58:25 -0800

    Added blank files for 5 teams

---
+++ Team1.cpp
@@ -0,0 +1,12 @@
+/*
+	Team 1 CV File
+	Version 0
+*/
+
+class VisionProcessing
+{
+	void Main()
+	{
+		// Insert Code
+	}
+}
---
+++ Team5.cpp
@@ -0,0 +1,12 @@
+/*
+	Team 2 CV File
+	Version 0
+*/
+
+class VisionProcessing
+{
+	void Main()
+	{
+		// Insert Code
+	}
+}
